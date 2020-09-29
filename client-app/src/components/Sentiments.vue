2cea4be20e160eb44f6ac6306662072cb56a873a
MANOUAN CEDRIC PASCAL-EMMANUEL <manouancedric@gmail.com>
2020-09-29 15:12:51 +0000

    adding vueJS

---
+++ client-app/src/components/Sentiments.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+//imports 
+import axios from 'axios';
+
+export default {
+  name: 'Sentiments',
+  data() {
+    return {
+      msg: 'Here is yout feeling!',
+    };
+  },
+
+  methods: {
+    getMessage() {
+      const path = 'http://localhost:5000/sentiments';
+      axios.get(path)
+        .then((res) => {
+          this.msg = res.data;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+        });
+    },
+  },
+  created() {
+    this.getMessage();
+  },
+};
+</script>
